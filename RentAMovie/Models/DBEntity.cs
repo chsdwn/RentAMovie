27e7d8dc9947a5897809bcea2904157e6a7d6112
koniia <hulusikafalier@gmail.com>
2019-04-24 13:37:57 +0300

    User table created. User login, register and manage features added.

--- RentAMovie/Models/DBEntity.cs
+++ RentAMovie/Models/DBEntity.cs
@@ -1,21 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace RentAMovie.Models
 {
-    public class DBEntity : DbContext
+    public class DBEntity : IdentityDbContext<ApplicationUser>
     {
-        public DBEntity() : base("DefaultConnection")
-        {
-
-        }
-
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<MembershipType> MembershipTypes { get; set; }
         public DbSet<Movie> Movies { get; set; }
+
+        public DBEntity() : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static DBEntity Create()
+        {
+            return new DBEntity();
+        }
     }
 }
