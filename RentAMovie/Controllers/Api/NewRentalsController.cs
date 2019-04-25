7aac70d938beee58e8488f2be8da347ae4dba358
koniia <hulusikafalier@gmail.com>
2019-04-25 12:44:36 +0300

    NewRentalDTO and NewRentalsController api created.

---
+++ RentAMovie/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,19 @@
+﻿using RentAMovie.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RentAMovie.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDTO newRentalDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RentAMovie/Dtos/NewRentalDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentAMovie.Dtos
+{
+    public class NewRentalDTO
+    {
+        public int Id { get; set; }
+        public List<int> MovieIds { get; set; }
+    }
+}
