eebc7b2568b3952549195a747f85d72db5d83d1c
koniia <hulusikafalier@gmail.com>
2019-04-25 14:08:47 +0300

    Custom typeahead js files merged.  Data grid table added for movies on new movie rental page.

---
+++ RentAMovie/Scripts/typeaheadRemote.js
@@ -0,0 +1,47 @@
+﻿var viewModel = {
+    movieIds: []
+};
+
+var customers = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+    url: '/api/customers?query=%QUERY',
+    wildcard: '%QUERY'
+    }
+});
+
+$('#customer').typeahead({
+    minLength: 3,
+    highlight: true
+    }, {
+    name: 'customers',
+    display: 'name',
+    source: customers
+    }).on("typeahead:select", function (e, customer) {
+        viewModel.customerId = customer.id;
+    });
+
+var movies = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+        url: '/api/movies?query=%QUERY',
+        wildcard: '%QUERY'
+    }
+});
+
+$('#movie').typeahead({
+    minLength: 3,
+    highlight: true
+}, {
+    name: 'movies',
+    display: 'name',
+    source: movies
+}).on("typeahead:select", function (e, movie) {
+    $("#movies").append("<li class='list-group-item'>" + movie.name + "</li>");
+
+    $("#movie").typeahead("val", "");
+
+    vm.movieIds.push(movie.id);
+});
